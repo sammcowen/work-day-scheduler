a4a4691288886c5690d2805d4c5546fae45cab42
HBK <sammcowen@gmail.com>
2022-01-21 11:54:40 -0800

    fixing iteration

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,10 @@
 
 var dateNow = moment().format("dddd, MMMM Do YYYY");
 //console.log(dateNow);
-var timeNow = moment().format("hmmA");
+var timeNow = moment().format("hA");
 console.log(timeNow);
+const hours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+
 
 //FUNCTIONS
 // displays current date 
@@ -11,21 +13,26 @@ $("#currentDay").text(dateNow);
 
 //  iterates through time blocks and changes bg-color
 // based on if past,present or future compared to current time
-$('.hour').each(function (index) {
-    if( this.text === timeNow) {
-        $('.col-8').addClass('present');
-    } else if(this.text < timeNow){
-        $('.col-8').addClass('past');
-    } else {
-        $('.col-8').addClass('future');
-    }
-    console.log( index + ": " + $( this ).text() );
-});
+$.each(hours, function(index, value) {
+    console.log(timeNow);
+    console.log(value);
+     if(hours.value !==  timeNow) {
+         console.log("you did it past");}
+        // $('.col-8').addClass('present');
+    // } else if(this.text < timeNow){
+        // $('.col-8').addClass('past');
+    // } else {
+        // $('.col-8').addClass('future');
+    // }
+    // console.log( index + ": " + $( this ).text() );
+//  }});
+
+ 
+  
 
 //INITIATORS
 
-   
-
-
-    
+    // $('.saveBtn').click(function() {
+    // console.log('saveBtn clicked');
+ });
 
