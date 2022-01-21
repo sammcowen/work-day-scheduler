0ca6a193d3b5b577ee8a16313964daaae1087dd1
HBK <sammcowen@gmail.com>
2022-01-20 22:19:49 -0800

    adding function to make text editable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,26 @@
+// VARIABLES 
+
 var dateNow = moment().format("dddd, MMMM Do YYYY");
 console.log(dateNow);
+var tasks = $('.text-area').textInput;
 
+//FUNCTIONS
 $("#currentDay").text(dateNow);
-
+// INITIATORS
+$('.text-area').click( function() {
+   console.log("yessir")
+   var textInput = $("<textarea>")
+  .addClass("form-control")
+  .val(tasks);
+  $(this).replaceWith(textInput);
+  textInput.trigger("focus");
+  $(".text-area").on("blur", "textarea", function() {
+      $(this).replaceWith(tasks);
+      var taskP = $("<div>")
+      .addClass("text-area")
+      .text(text);
+    
+    // replace textarea with p element
+    $(this).replaceWith(taskP);
+});
+});
