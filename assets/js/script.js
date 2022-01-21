56a8de5faa65e220815a9939f0bb6eaa343e4ef9
HBK <sammcowen@gmail.com>
2022-01-21 13:02:56 -0800

    iterate over cols and display diff bg colors based on time

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,37 +2,39 @@
 
 var dateNow = moment().format("dddd, MMMM Do YYYY");
 //console.log(dateNow);
-var timeNow = moment().format("hA");
+var timeNow = Number(moment().format("H"));
 console.log(timeNow);
-const hours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
-
+const hours = $('.hour');
+//INITIATORS
+ $('.saveBtn').click(function() {
+    console.log('start button clicked');
+ });
+ 
 
 //FUNCTIONS
 // displays current date 
 $("#currentDay").text(dateNow);
 
+
+
 //  iterates through time blocks and changes bg-color
 // based on if past,present or future compared to current time
 $.each(hours, function(index, value) {
-    console.log(timeNow);
-    console.log(value);
-     if(hours.value !==  timeNow) {
-         console.log("you did it past");}
-        // $('.col-8').addClass('present');
-    // } else if(this.text < timeNow){
-        // $('.col-8').addClass('past');
-    // } else {
-        // $('.col-8').addClass('future');
-    // }
-    // console.log( index + ": " + $( this ).text() );
-//  }});
+   // console.log(timeNow);
+    console.log(Number(value.dataset.hour));
+    console.log(Number(value.dataset.hour) > timeNow);
+     if(Number(value.dataset.hour) > timeNow) {
+      $(this).next().addClass('future');   
+     }else if(Number(value.dataset.hour) === timeNow){
+         $(this).next().addClass('present');
+     } else {
+         $(this).next().addClass('past');
+     }
+    });
+        
+
 
  
   
 
-//INITIATORS
-
-    // $('.saveBtn').click(function() {
-    // console.log('saveBtn clicked');
- });
 
